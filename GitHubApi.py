9567327ab95a39307eef4ee81ff1e0ad0f1b558c
Kipsy Quevada <quekipz@gmail.com>
2018-10-04 21:29:00 -0400

    Edit code to include mocks

--- GitHubApi.py
+++ GitHubApi.py
@@ -2,9 +2,13 @@
 Kipsy Quevada
 SSW-567-WS: SW Testing, Qual. Assur. & Maint
 Fall 2018 | Professor Rich Kempinski
-HW 04a: Develop with the Perspective of the Tester in mind
-The goal of this assignment is to approach it as a developer who more
-than anything else has the perspective of the tester in the front of their mind. 
+HW 05a: Isolate External Dependencies
+In this assignment you will use a mocking package
+to "mock" your program's external dependence on
+GitHub, so that you can guarantee that your unit
+tests will run consistently ever time you run them,
+no matter how many times you run them, and no matter
+what changes you make to your repos.
 AMDG
 """
 #---------------------------------------------------------------------------
@@ -16,12 +20,24 @@ def getReposAndCommits(user):
     """ Take as input a GitHub user ID. The output from the function will be a
     list of the names of the repositories that the user has, along with the
     number of commits that are in each of the listed repositories. """
+    results = []
     r = requests.get('https://api.github.com/users/' + user + '/repos')
-    repos = json.loads(r.content.decode('utf-8'))
+    repos = r.json()
     if 'message' in repos: # message: Not found will appear in dict if user is invalid
-        yield "Error: User not found. Please try again."
+        results.append(["Error", 0])
     else:
         for rep in repos:
             c = requests.get('https://api.github.com/repos/' + user + '/' + rep['name'] + '/commits')
-            commits = json.loads(c.content.decode('utf-8'))
-            yield "Repo: " + rep['name'] + " Number of commits: " + str(len(commits))
+            commits = c.json()
+            results.append([rep['name'], str(len(commits))])
+    return results
+
+def yieldReposAndCommits(user):
+    data_list = getReposAndCommits(user)
+    if data_list[0][0] == "Error":
+        yield 'Error: User not found. Please try again.'
+    else:
+        for item in data_list:
+            yield "Repo: " + item[0] + " Number of commits: " + item[1]
+
+# print(getReposAndCommits('richkempinski'))
--- TestGitHubApi.py
+++ TestGitHubApi.py
@@ -2,30 +2,64 @@
 Kipsy Quevada
 SSW-567-WS: SW Testing, Qual. Assur. & Maint
 Fall 2018 | Professor Rich Kempinski
-HW 04a: Develop with the Perspective of the Tester in mind
-The goal of this assignment is to approach it as a developer who more
-than anything else has the perspective of the tester in the front of their mind. 
+HW 05a: Isolate External Dependencies
+In this assignment you will use a mocking package
+to "mock" your program's external dependence on
+GitHub, so that you can guarantee that your unit
+tests will run consistently ever time you run them,
+no matter how many times you run them, and no matter
+what changes you make to your repos.
 AMDG
 """
 #---------------------------------------------------------------------------
 
 import unittest
-
-from GitHubApi import getReposAndCommits
+from GitHubApi import getReposAndCommits, yieldReposAndCommits
+from unittest.mock import patch, MagicMock as Mock
+import json
 
 class TestGetReposAndCommits(unittest.TestCase):
     """ test getReposAndCommits function """
-    def testInvalidUser(self):
-        results = list()
-        for line in getReposAndCommits(''):
-            results.append(line)
-        self.assertEqual(results, ['Error: User not found. Please try again.'], 'Any invalid user results in error message.')
-    def testRichKempinski(self):
-        results = list()
-        for line in getReposAndCommits('richkempinski'):
-            results.append(line)
-        self.assertEqual(results, ['Repo: hellogitworld Number of commits: 30', 'Repo: helloworld Number of commits: 2', 'Repo: Project1 Number of commits: 2', 'Repo: threads-of-life Number of commits: 1'], 'Richkempinski has 4 repos.')
+    
+    # def testInvalidUser(self):
+    #     results = list()
+    #     for line in yieldReposAndCommits(''):
+    #         results.append(line)
+    #     self.assertEqual(results, ['Error: User not found. Please try again.'], 'Any invalid user results in error message.')
+    
+    # def testRichKempinski(self):
+    #     results = list()
+    #     for line in yieldReposAndCommits('richkempinski'):
+    #         results.append(line)
+    #     self.assertEqual(results, ['Repo: hellogitworld Number of commits: 30', 'Repo: helloworld Number of commits: 2', 'Repo: Mocks Number of commits: 3', 'Repo: Project1 Number of commits: 2', 'Repo: threads-of-life Number of commits: 1'], 'Richkempinski has 5 repos.')
     # Note: Originally included my own user 'quekipz' but realized that my commit count increases when I push the assignment, which fails the test.
+    
+    @patch('requests.get')
+    def testInvalidUserMock(self, injectedMock):
+        final_results = list()
+        results = [Mock(), Mock()]
+        results[0].json.return_value = {"message": "Not Found", "documentation_url": "https://developer.github.com/v3"}
+        results[1].json.return_value = {"message": "Not Found", "documentation_url": "https://developer.github.com/v3"}
+        injectedMock.side_effect = results
+        for line in yieldReposAndCommits(''):
+            final_results.append(line)
+        self.assertEqual(final_results, ['Error: User not found. Please try again.'], 'Any invalid user results in error message.')
+
+    @patch('requests.get')
+    def testRichKempinskiMock(self, injectedMock):
+        final_results = list()
+        results = [Mock(), Mock(), Mock(), Mock(), Mock(), Mock()]
+        results[0].json.return_value = [{"name": "hellogitworld"}, {"name": "helloworld"}, {"name": "Mocks"}, {"name": "Project1"}, {"name": "threads-of-life"}]
+        results[1].json.return_value = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+        results[2].json.return_value = [{}, {}]
+        results[3].json.return_value = [{}, {}, {}]
+        results[4].json.return_value = [{}, {}]
+        results[5].json.return_value = [{}]
+        injectedMock.side_effect = results
+        for line in yieldReposAndCommits('richkempinski'):
+            final_results.append(line)
+        self.assertEqual(final_results, ['Repo: hellogitworld Number of commits: 30', 'Repo: helloworld Number of commits: 2', 'Repo: Mocks Number of commits: 3', 'Repo: Project1 Number of commits: 2', 'Repo: threads-of-life Number of commits: 1'], 'Richkempinski has 5 repos.')
+
 
 
 if __name__ == '__main__':
